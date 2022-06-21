a93d57fd35da46e9da5777754f580d1f94963a6d
bkee9 <nancy.mwende@student.moringaschool.com>
2022-06-21 10:45:47 +0300

    ft:add tests file

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business = Business(name='Test Business')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))
+
+    def test_save_method(self):
+        self.business.save()
+        businesses = Business.objects.all()
+        self.assertTrue(len(businesses) > 0)
+
+    def test_delete_method(self):
+        self.business.save()
+        self.business.delete_business()
+        businesses = Business.objects.all()
+        self.assertTrue(len(businesses) == 0)
+
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post = Post(title='Test Post')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_method(self):
+        self.post.save()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+    def test_delete_method(self):
+        self.post.save()
+        self.post.delete_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) == 0)
